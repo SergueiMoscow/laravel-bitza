0bc251a9c8557206f9e93e2751a6518efe75e45c
serguei <s_serguei@rambler.ru>
2023-02-02 01:01:27 +0300

    add modal window for edit contact (not terminated)

--- public/js/bitza.js
+++ public/js/bitza.js
@@ -17,20 +17,82 @@ function post(url, data = {}) {
     }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var input = document.getElementById("search_contact")
-    input.oninput = function(event) {
+    input.oninput = function (event) {
         //data = post('/contacts/search', {'q': input.value});
         console.log(event.srcElement.value);
         var xhr = new XMLHttpRequest();
-        xhr.open("GET", '/contacts/search?q='+event.srcElement.value, true);
-        xhr.responseType = 'text/html';
+        xhr.open("GET", '/contacts/search?q=' + event.srcElement.value, true);
+        xhr.responseType = 'text';
         xhr.send();
-        xhr.onload = function(){
+        xhr.onload = function () {
             divListContact = document.getElementById("list_contact");
-            divListContact.innerHTML=xhr.response;
+            divListContact.innerHTML = xhr.response;
+        }
     }
 
-        
+    const modal = document.querySelector('#myModal');
+
+    // назначаем обработчик события для клика по кнопке открытия окна
+    document.querySelector('#editBtn').addEventListener('click', openModal);
+
+    /*
+     * Обработчик события клика по кнопке открытия модального окна
+     */
+    function openModal() {
+        modal.classList.add('modal-open');
+        // обработчики событий, которые работают, когда окно открыто
+        attachModalEvents();
     }
+
+    /*
+     * Функция назначает обработчики событий к элементам модального окна при открытии
+     */
+    function attachModalEvents() {
+        // закрывать модальное окно при нажатии на крестик
+        modal.querySelector('.close').addEventListener('click', closeModal);
+        // закрывать модальное окно при нажатии клавиши Escape
+        document.addEventListener('keydown', handleEscape);
+        // закрывать модальное окно при клике вне контента модального окна
+        modal.addEventListener('click', handleOutside);
+    }
+
+    /*
+     * Обработчик события клика по кнопке закрытия модального окна
+     */
+    function closeModal() {
+        modal.classList.remove('modal-open');
+        // окно закрыто, эти обработчики событий больше не нужны
+        detachModalEvents();
+    }
+
+    /*
+     * Функция удаляет обработчики событий к элементам модального окна при закрытии
+     */
+    function detachModalEvents() {
+        modal.querySelector('.close').removeEventListener('click', closeModal);
+        document.removeEventListener('keydown', handleEscape);
+        modal.removeEventListener('click', handleOutside);
+    }
+
+    /*
+     * Функция закрывает модальное окно при нажатии клавиши Escape
+     */
+    function handleEscape(event) {
+        if (event.key === 'Escape') {
+            closeModal();
+        }
+    }
+
+    /*
+     * Функция закрывает модальное окно при клике вне контента модального окна
+     */
+    function handleOutside(event) {
+        const isClickInside = !!event.target.closest('.modal-content');
+        if (!isClickInside) {
+            closeModal();
+        }
+    }
+
 });
--- resources/views/bitza/contacts/index.blade.php
+++ resources/views/bitza/contacts/index.blade.php
@@ -1,7 +1,18 @@
 <x-app-layout>
+<!-- Затемнение всей страницы при открытии окна -->
+    <div id="myModal" class="modal">
+        <!-- Содержимое модального окна -->
+        <div class="modal-content">
+            <span class="close">&times;</span>
+            <p>Контент модального окна</p>
+        </div>
+    </div>
     <div class="content">
         <div class="search">
             <input class="border-gray-300 rounded-md w-full" type="text" id="search_contact" />
+            <button id="editBtn" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150 ml-3">+</button>
+        </div>
+        <div class="edit">
         </div>
         <div id="list_contact" class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             @include('bitza.contacts.list')
